bfab172e90baa38a91d8daa5b898892b1483a023
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-09-16 12:11:07 +0900

    rte, 文分割まで

---
+++ rte_in_nltk.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 16 11:55:55 2017
+
+@author: Takanori
+"""
+
+import nltk
+
+# 利用する仮説について
+rtepair = nltk.corpus.rte.pairs(['rte3_dev.xml'])[33]
+
